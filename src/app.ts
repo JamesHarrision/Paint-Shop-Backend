1c4c937817e3369fabe6629575191b36cf6ca48a
Jack1e <djack1e2401@gmail.com>
2026-01-10 19:39:43 +0700

    Cai dat prisma client, #PLEASE use prisma version 5.22

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import helmet from 'helmet';
 import cors from 'cors'
+import { prisma } from './config/prisma';
 
 const app = express();
 
@@ -10,11 +11,21 @@ app.use(cors());
 app.use(express.json());
 
 //Test Route
-app.get('/', (req, res) => {
-  res.status(200).json({
-     message: 'Welcome to Paint Shop API 🚀',
-     status: 'success'
-  });
+app.get('/', async (req, res) => {
+  try {
+    const userCount = await prisma.user.count();
+    res.status(200).json({
+      message: 'Welcome to Paint Shop API',
+      database: 'Connected to mySQL',
+      userCount: userCount
+    });
+  }
+  catch (error) {
+    res.status(500).json({
+      messege: 'Database connection failed',
+      error: error
+    })
+  }
 });
 
 export default app
