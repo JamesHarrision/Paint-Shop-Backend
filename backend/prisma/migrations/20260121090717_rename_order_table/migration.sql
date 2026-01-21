75d0f43c639cd6f8ab42023e266fbc99e9db0897
Jack1e <djack1e2401@gmail.com>
2026-01-21 16:07:53 +0700

    rename order model

---
+++ backend/prisma/migrations/20260121090717_rename_order_table/migration.sql
@@ -0,0 +1,32 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Order` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE `Order` DROP FOREIGN KEY `Order_userId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `order_items` DROP FOREIGN KEY `order_items_orderId_fkey`;
+
+-- DropTable
+DROP TABLE `Order`;
+
+-- CreateTable
+CREATE TABLE `orders` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER NOT NULL,
+    `totalAmount` DECIMAL(10, 2) NOT NULL,
+    `status` ENUM('PENDING', 'COMPLETED', 'CANCELLED') NOT NULL DEFAULT 'PENDING',
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `orders` ADD CONSTRAINT `orders_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `order_items` ADD CONSTRAINT `order_items_orderId_fkey` FOREIGN KEY (`orderId`) REFERENCES `orders`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
